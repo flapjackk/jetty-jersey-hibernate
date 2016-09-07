78714e8b2c5d34682fbe362164c67ef60da5300c
lszita <lajos.szita@gmail.com>
2016-09-07 07:26:41 +0200

    fixed typeo

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -17,8 +17,7 @@ public class HelloServlet extends HttpServlet
     {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Go odnight Servlet yolo</h1>");
-        response.getWriter().println("<p>some random paragraph</p>");
+        response.getWriter().println("<h1>Hello world!</h1>");
         response.getWriter().println("session=" + request.getSession(true).getId());
     }
 }
